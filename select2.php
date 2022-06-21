4d8701d1a40fb8874ebc18089451e5516193eab6
arseny <43606985+ArsenyOK@users.noreply.github.com>
2022-06-21 18:23:36 +0300

    Update select2.php

--- select2.php
+++ select2.php
@@ -1,22 +1,16 @@
 <?php
-
+header('Content-Type: text/xml');
+header('Cache-Control: no-cache, must-revalidate'); 
 include "./connection.php";
-
-
-$name_actor = $_GET["name_actor"];
-// $name = "Лоретта Дивайн"
-
-$sth = $connection->prepare('SELECT f.name from film f, film_actor, actor a
-where ID_Film=FID_Film and FID_Actor=ID_Actor and a.name=?');
-$sth->execute(array($name_actor));
-$res = $sth->fetchAll();
-
-if($res) {
-    foreach ($res as $row) {
-        echo "<li>$row[0]</li>";
+    echo '<?xml version="1.0" encoding="UTF-8"?>';
+    echo "<root>";
+    $name_actor = $_GET['name_actor']; 
+    $sqlSelect = $connection->prepare('SELECT f.name from film f, film_actor, actor a
+    where ID_Film=FID_Film and FID_Actor=ID_Actor and a.name=?');
+    $sqlSelect->execute(array($name_actor));
+    while ($cell = $sqlSelect->fetch(PDO::FETCH_BOTH)) {
+      $item = $cell[0];
+      echo "<row><item> $item </item></row>";
     }
-} else {
-    echo "<p>Not found</p>";
-}
-
+    echo "</root>"
 ?>
