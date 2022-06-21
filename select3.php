09c4fd94e00b6059176a501a542d8f03b4d37f7b
arseny <43606985+ArsenyOK@users.noreply.github.com>
2022-06-21 18:24:19 +0300

    select3 changed

--- select3.php
+++ select3.php
@@ -1,21 +1,9 @@
 <?php
+    include "./connection.php";
+    $user_date = $_GET["user_date"];
+    $sth = $connection->prepare('SELECT name from film where date BETWEEN ? and CURRENT_DATE');
 
-include "./connection.php";
-
-
-$user_date = $_GET["user_date"];
-// $name = "Лоретта Дивайн"
-
-$sth = $connection->prepare('SELECT name from film where date BETWEEN ? and CURRENT_DATE');
-$sth->execute(array($user_date));
-$res = $sth->fetchAll();
-
-if($res) {
-    foreach ($res as $row) {
-        echo "<li>$row[0]</li>";
-    }
-} else {
-    echo "<p>Not found</p>";
-}
-
-?>
+    $sth->execute(array($user_date)); 
+    $cell = $sth->fetchAll(PDO::FETCH_OBJ);
+    echo json_encode($cell[0]);
+    ?>
